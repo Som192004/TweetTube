b2aa7276d09567bf38b96a840b51a1839c487cd4
Soham <sohamholkar800@gmail.com>
2024-09-28 19:29:16 +0530

    All routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {upload} from "../middlewares/uploadFile.middleware.js"
-import {registerUser , login, logoutUser} from "../controllers/user.controller.js";
+import {registerUser , login, logoutUser, updateUserPassword, updateAvatar} from "../controllers/user.controller.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router() 
@@ -21,6 +21,22 @@ router.route("/login").post(login)
 router.route("/logout").post(verifyJWT , logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT , updateUserPassword)
+
+router.route("/profile").get(verifyJWT , getUserInfo)
+
+router.route("/update-user-info").patch(verifyJWT , updateAccountInfo)
+
+router.route("/update-user-avatar").patch(verifyJWT , upload.single("avatar") , updateAvatar)
+
+router.route("/cover-img" , verifyJWT , upload.single("/coverImage") , updateCoverImg)
+
+router.route("/channel/:username").get(verifyJWT , getUserChannelInfo)
+
+router.route("/watch-history").get(verifyJWT , getWatchHistory)
+
+
 
 export default router 
 
